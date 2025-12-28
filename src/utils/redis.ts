f300ffe6fb9410c73df62774e919bcf50113954d
RohanMalakar <rohanmalakar5091@gmail.com>
2025-12-28 13:43:24 +0530

    add the env variable

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,11 +1,11 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-    username: 'default',
-    password: 'Z8sgldLAMU5cpMXejRVY1d8KVgHcpf6v',
+    username: process.env.REDIS_USERNAME || 'default',
+    password: process.env.REDIS_PASSWORD || 'Z8sgldLAMU5cpMXejRVY1d8KVgHcpf6v',
     socket: {
-        host: 'redis-13532.c241.us-east-1-4.ec2.cloud.redislabs.com',
-        port: 13532
+        host: process.env.REDIS_HOST || 'redis-13532.c241.us-east-1-4.ec2.cloud.redislabs.com',
+        port: parseInt(process.env.REDIS_PORT || '13532')
     }
 });
 
